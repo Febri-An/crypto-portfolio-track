f576683622a9f62d8d7871442b633c2a4471adb8
Febri An <febri.anjy.nirwana.354@gmail.com>
2024-08-29 07:09:57 +0700

    fix: correct typo in fucntion name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Header() {
+export default function Footer() {
     return (
         <div className="footer">
             <footer>Copyright © 2024</footer>
