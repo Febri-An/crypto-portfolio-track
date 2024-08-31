5d407754b865790c459ae36fd2622d664a844ec3
Febri An <febri.anjy.nirwana.354@gmail.com>
2024-08-31 09:05:14 +0700

    Updates

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-    return (
-        <div className="footer">
-            <footer>Copyright © 2024</footer>
-        </div>
-    )
-}
